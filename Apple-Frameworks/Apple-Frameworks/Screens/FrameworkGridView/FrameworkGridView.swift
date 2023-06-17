2c9e3e9b033be33dd47a71dc549c484278e25f22
Thomas Williams <thwilliams4129@gmail.com>
2023-06-16 21:53:29 -0400

    Completed List version

--- Apple-Frameworks/Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -13,22 +13,17 @@ struct FrameworkGridView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: viewModel.columns) {
-                    ForEach(MockData.frameworks) { framework in
+            List {
+                ForEach(MockData.frameworks) { framework in
+                    NavigationLink(destination: FrameworkDetailView(framework: framework, isShowingDetailView: $viewModel.isShowingDetailView)) {
                         FrameworkTitleView(framework: framework)
-                            .onTapGesture {
-                                viewModel.selectedFramework = framework
-                            }
                     }
+                    
                 }
             }
             .navigationTitle("🍎 Frameworks")
-            .sheet(isPresented: $viewModel.isShowingDetailView) {
-                FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFrameWork,
-                                    isShowingDetailView: $viewModel.isShowingDetailView)
-            }
         }
+        .accentColor(Color(.label))
     }
 }
 
